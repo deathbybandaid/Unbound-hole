9f999a8a39dd37e2a2633d5033e1d26eae6b4c99
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2019-03-07 10:57:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,9 @@ ROOTSURL=https://www.internic.net/domain/named.root
 # temporary download location for root.hints
 TEMPROOTS=/tmp/root.hints
 
+# location to implant root.hints file
+CURRENTROOTS=/var/lib/unbound/root.hints
+
 # Pi-hole installation detection
 PIHOLEDIR=/etc/pihole
 PIHOLECONF="${PIHOLEDIR}/setupVars.conf"
@@ -42,9 +45,50 @@ else
   exit 1
 fi
 
+# Pull Pi-hole setup vars
 source $PIHOLECONF
 
-echo $IPV4_ADDRESS
+# Install unbound
+if which unbound >/dev/null;
+then
+  echo "Unbound is already installed"
+else
+  echo "Installing Unbound"
+  apt-get install -y unbound
+fi
 
-echo "grrr"
+# Install root.hints file
+if [[ -f $CURRENTROOTS ]]
+then
+  echo "Checking existing file"
+  SOURCEMODIFIEDLAST=$(curl --silent --head $ROOTSURL | awk -F: '/^Last-Modified/ { print $2 }')
+  SOURCEMODIFIEDTIME=$(date --date="$SOURCEMODIFIEDLAST" +%s)
+  LOCALFILEMODIFIEDLAST=$(stat -c %z "$CURRENTROOTS")
+  LOCALFILEMODIFIEDTIME=$(date --date="$LOCALFILEMODIFIEDLAST" +%s)
+  if [[ $LOCALFILEMODIFIEDTIME -lt $SOURCEMODIFIEDTIME ]]
+  then
+    DOWNLOADFRESH=true
+    echo "File updated online"
+  else
+    echo "File not updated online"
+  fi
+else
+  DOWNLOADFRESH=true
+  echo "File Missing"
+fi
 
+
+if [[ $DOWNLOADFRESH = true ]]
+then
+  echo "Attempting to download file"
+  wget -O $TEMPROOTS $ROOTSURL
+  FETCHFILESIZE=$(stat -c%s $TEMPROOTS)
+  if [[ $FETCHFILESIZE -gt 0 ]]
+  then
+     mv $TEMPROOTS $CURRENTROOTS
+  else
+    echo "File download failed"
+  fi
+else
+  echo "Not downloading file"
+fi

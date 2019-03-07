19af927795cbafa538c82666bcb07f88c6c5fb82
Deathbybandaid <deathbybandaid@users.noreply.github.com>
2019-03-07 10:24:28 -0500

    Update update_script.sh

--- update_script.sh
+++ update_script.sh
@@ -1,23 +1,41 @@
 #!/bin/bash
 
 SOURCEURL=https://www.internic.net/domain/named.root
-
 CURRENTFILE=/var/lib/unbound/root.hints
 TEMPFILE=/home/root.hints
+DOWNLOADFRESH=false
 
-
-SOURCEMODIFIEDLAST=$(curl --silent --head $SOURCEURL | awk -F: '/^Last-Modified/ { print $2 }')
-SOURCEMODIFIEDTIME=$(date --date="$SOURCEMODIFIEDLAST" +%s)
-
-LOCALFILEMODIFIEDLAST=$(stat -c %z "$CURRENTFILE")
-LOCALFILEMODIFIEDTIME=$(date --date="$LOCALFILEMODIFIEDLAST" +%s)
-
-if [[ $LOCALFILEMODIFIEDTIME -lt $SOURCEMODIFIEDTIME ]]
+if [[ -f $CURRENTFILE ]]
 then
+  echo "Checking existing file"
+  SOURCEMODIFIEDLAST=$(curl --silent --head $SOURCEURL | awk -F: '/^Last-Modified/ { print $2 }')
+  SOURCEMODIFIEDTIME=$(date --date="$SOURCEMODIFIEDLAST" +%s)
+  LOCALFILEMODIFIEDLAST=$(stat -c %z "$CURRENTFILE")
+  LOCALFILEMODIFIEDTIME=$(date --date="$LOCALFILEMODIFIEDLAST" +%s)
+  if [[ $LOCALFILEMODIFIEDTIME -lt $SOURCEMODIFIEDTIME ]]
+  then
+    DOWNLOADFRESH=true
+    echo "File updated online"
+  else
+    echo "File not updated online"
+  fi
+else
+  DOWNLOADFRESH=true
+  echo "File Missing"
+fi
+
+
+if [[ $DOWNLOADFRESH = true ]]
+then
+  echo "Attempting to download file"
   wget -O $TEMPFILE $SOURCEURL
   FETCHFILESIZE=$(stat -c%s $TEMPFILE)
   if [[ $FETCHFILESIZE -gt 0 ]]
   then
      mv $TEMPFILE $CURRENTFILE
+  else
+    echo "File download failed"
   fi
+else
+  echo "Not downloading file"
 fi
